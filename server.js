fbfc0b30c7432e6511265ceb9b403784e13cf822
Leona Kokerai <150783140+leeownuh@users.noreply.github.com>
2026-03-11 11:31:17 +0530

    redirecting etc

--- server.js
+++ server.js
@@ -106,8 +106,10 @@ router.post('/admin/login', async (req, res) => {
     const { username, password } = req.body;
     const path = require('path');
 
-    const adminData = readJSON(path.join(process.cwd(), 'data', 'admin.json'));
-   console.log("Admin path:", path.join(process.cwd(), 'data', 'admin.json'));
+    const adminData = readJSON(
+      path.join(__dirname, 'data/admin.json')
+    );
+   console.log("Admin path:", path.join(__dirname, 'data/admin.json'));
 console.log("Admin file:", adminData);
     // Check username
     if (username !== adminData.username) {
